d7e5cb8cee9c8d5f56c3f0df10f8b6e7fe14b8fd
Ethan Fung <EthanEFung@gmail.com>
2017-10-26 10:57:22 -0700

    Rid Timekit memory leak
    Found the timekitDB.js file left in the db repository that is
    required in the server's index.js.  This file serves no purpose
    but instead takes up space on our server that could be easily
    delegated to other more pressing matters. Deleted file and deleted
    line of code that requires the file into index.js.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const db = require('../db/config');
-const express = require('express');
-const models = require('../db/index');
-const path = require('path');
-const routes = require('./routes/index');
-const timekitDB = require('../db/timekitDB.js');
-const $ = require('jquery');
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("../db/config");
+const express = require("express");
+const models = require("../db/index");
+const path = require("path");
+const routes = require("./routes/index");
+const $ = require("jquery");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,16 +13,16 @@ const app = express()
   .use(cors())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
-  .use(express.static(path.resolve(__dirname, '../client/public')))
-  .use('/api', routes);
+  .use(express.static(path.resolve(__dirname, "../client/public")))
+  .use("/api", routes);
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'))
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
 app.listen(PORT, err => {
   if (err) {
     console.log(`Error connecting to server ${err}`);
-  } 
+  }
   console.log(`Listening on PORT ${PORT}`);
 });
